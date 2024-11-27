de2c3c8aa5a192dc8f70d5e39e7258964b1d6db0
sanoxion <bustimakki0001@gmail.com>
2024-11-27 17:43:26 +0500

    Составление Функции 3

--- laba2/main.cpp
+++ laba2/main.cpp
@@ -15,8 +15,15 @@ void print(const vector<string>& lines) {
         cout << str << endl;
     }
 
-void write(const vector<string>& lines, const string& filename) {
-}
+void writeToFile(const vector<string>& lines, const string& filename) {
+    ofstream file(filename);
+    if (!file) {
+        cerr << "Ошибка открытия файла для записи!" << endl;
+        return;
+    }
+    for (const auto& str : lines) {
+        file << str << endl;
+    }
 
 int main() {
 
