773a41f823a0ac79b64098b0b4a99f7d7cbc4f3a
UraAkonka <lebzyak321@mail.ru>
2024-11-27 17:12:56 +0500

    Добавлены каркасы функций

--- laba.cpp
+++ laba.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
+// function2.cpp
+void print(const vector<string>& lines) {
+    }
+
+
+// function3.cpp
+void write(const vector<string>& lines, const string& filename) {
+}
+
+
 int main() {
-    cout << "I love coding!" << endl;
+    cout << "" << endl;
     return 0;
 }
